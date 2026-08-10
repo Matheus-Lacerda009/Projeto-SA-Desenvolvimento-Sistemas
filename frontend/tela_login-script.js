b84d7cb2deb73fc42a7a5b6758912af5079b0a4d
ArtMainardi <iensenmainardi@gmail.com>
2026-08-10 18:52:08 -0300

    feat: adiciona sistema de login

--- frontend/tela_login-script.js
+++ frontend/tela_login-script.js
@@ -6,7 +6,30 @@ loginButton.addEventListener('click', async function(){
     const feedback = document.getElementById("feedback");
     feedback.innerHTML = "";
 
+    // Verifica dados:
     if(email == "" || senha == ""){
         feedback.innerHTML = "Preencha todos os dados para poder fazer login!";
+    }
+
+    // Permite liberação offline para usuario administrador:
+    if(email == "adm@dev.com" || senha == "aaa"){
+        feedback.innerHTML = "Login realizado com sucesso!";
+    }
+
+    try{
+        // Manda requisição:
+        const API = await fetch(`http://localhost:8085/usuario?identificador=${email}&senha=${senha}`, {
+            method: "GET"
+        });
+        const resposta = await API.json();
+
+        // Verifica a resposta:
+        if(resposta.validacao){
+            feedback.innerHTML = "Login realizado com sucesso!";
+        } else{
+            feedback.innerHTML = "Falha no login!";
+        }
+    } catch(erro){
+        feedback.innerHTML = erro;
     }
 });
