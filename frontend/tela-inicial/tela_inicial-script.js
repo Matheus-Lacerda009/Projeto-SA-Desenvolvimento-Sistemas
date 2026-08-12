632b7459fce2263046ea4e55378338c236c5da62
ArtMainardi <iensenmainardi@gmail.com>
2026-08-11 21:19:45 -0300

    feat: adiciona pesquisa de conteúdos

--- frontend/tela-inicial/tela_inicial-script.js
+++ frontend/tela-inicial/tela_inicial-script.js
@@ -1,6 +1,8 @@
-window.addEventListener("load", async function(){
-    const videosContainer = document.getElementById("videos");
+const videosContainer = document.getElementById("videos");
 
+window.addEventListener("load", carregar());
+
+async function carregar(){
     try{
         // Faz requisição:
         const API = await fetch(`http://localhost:8085/filme`, {
@@ -26,11 +28,11 @@ window.addEventListener("load", async function(){
     } catch(error){
         videosContainer.innerHTML = `<span>Erro: ${error}</span>`;
     }
-});
+}
 
 function listar(lista){
     let code = "";
-
+    
     lista.forEach(conteudo => {
         code += `
             <div class="div" data-id=${conteudo.id_filme}>
@@ -44,6 +46,45 @@ function listar(lista){
 
     return code;
 }
+
+
+// Realiza pesquisa pelo nome:
+const input = document.getElementById("pesquisa-nome");
+input.addEventListener("input", async function(){
+    const busca = input.value;
+    console.log(busca)
+
+    if(busca == ""){
+        carregar();
+        return;
+    }
+
+    try{
+        const API = await fetch(`http://localhost:8085/filme/busca?nome=${busca}`, {
+            method : "GET"
+        });
+        const resposta = await API.json();
+
+        if(resposta.length == 0){
+            videosContainer.innerHTML = "<span>Nenhum vídeo com esse nome encontrado!</span>";
+        }else{
+            console.log(resposta)
+            // Adiciona elementos na tela:
+            const code = listar(resposta);
+            videosContainer.innerHTML = code;
+            // Configura sistemas para detalhes:
+            const conteudos = document.querySelectorAll(".div");
+            conteudos.forEach(c => {
+                c.addEventListener("click", (e) => {
+                    const id = e.currentTarget.dataset.id;
+                    window.location.href = `../pagina-detalhes/detalhes.html?id=${id}`;
+                })
+            });
+        }
+    } catch(error){
+        videosContainer.innerHTML = `<span>Erro: ${error}</span>`;
+    }
+});
 
 document.getElementById("logout").addEventListener("click", () => {
     window.location.replace("../tela_login.html");
